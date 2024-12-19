3ae9ac0ad465c64a463617de6da7eac7c53dfe5c
Alexis Doualle <adoualle@gmail.com>
2024-12-19 12:26:31 +0100

    Initial Commit

---
+++ LazyCanvasZoom/LazyCanvasZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyCanvasZoomApp.swift
+//  LazyCanvasZoom
+//
+//  Created by Alexis Doualle on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LazyCanvasZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LazyCanvasZoomTests/LazyCanvasZoomTests.swift
@@ -0,0 +1,16 @@
+//
+//  LazyCanvasZoomTests.swift
+//  LazyCanvasZoomTests
+//
+//  Created by Alexis Doualle on 19/12/2024.
+//
+
+import Testing
+
+struct LazyCanvasZoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
